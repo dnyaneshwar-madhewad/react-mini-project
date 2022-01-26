1602f1bcf14bc90c48ce2427482a710813aa2117
dnyaneshwar-madhewad <itz.dnyaneshwar@gmail.com>
2022-01-26 12:49:06 +0530

    b2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,26 @@
-// JSX => JAVA SCRIPT XML
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div>
+      <Hello />
+      <Hello />
+      <Hello />
+      <World />
+      <World />
+    </div>
+  );
+}
+
+// JSX :: REACT COMPOANT
+// Hello();
+// Hello();
+// <Hello />
+function Hello() {
+  return <h1>Hello! </h1>;
+}
+
+// World();
+// <World />
+// JSX => X => XML
+function World() {
+  return <h1>World!</h1>;
 }
