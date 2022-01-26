0cd5ab9b5a81c842428a75eca40de69919d1efee
dnyaneshwar-madhewad <itz.dnyaneshwar@gmail.com>
2022-01-26 12:53:17 +0530

    b4

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 export default function App() {
   return (
     <div>
-      <Hello id="1" name="Dnyaneshwar" />
-      <Hello id="2" name="sachin" />
-      <Hello id="2" name="virat" />
+      <Hello />
+      <Hello name="rahul" />
+      <Hello id="2" name="Dnyaneshwar" />
     </div>
   );
 }
 
-// Hello();
-// Hello(1);
-// Hello(1, 2);
-// <Hello />
-function Hello(p1) {
-  console.log(p1);
+/*function Hello() {
+  return <h1>Hello!  </h1>;
+}*/
 
+/*function Hello(p1) {
   return <h1>Hello! {p1.name} </h1>;
+}*/
+
+// with descruting
+function Hello({ name, id }) {
+  return <h1>Hello! {name} </h1>;
 }
